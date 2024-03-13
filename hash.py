846efbf5057f2145bcb0741e796676c8739b9d32
asthary23 <154309720+asthary23@users.noreply.github.com>
2024-03-13 13:48:59 -0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -5,7 +5,7 @@ def memoize(f):
     # This inner function has access to memory and 'f'
     def inner(num): 
         if num not in memory:          
-            memory[num] = f.board(num) 
+            memory[num] = f(num) 
         return memory[num] 
     return inner
 
